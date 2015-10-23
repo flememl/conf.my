087417fb5dd9c80508af967b5ac1dac43c5654d9
Quentin Roquefort <flememl@gmail.com>
2015-10-23 10:59:43 +0100

    adding submodules for go/yaml modes

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -s ~/Public/conf.my/bashrc ~/.bashrc
+ln -s ~/Public/conf.my/emacs ~/.emacs
+ln -s ~/Public/conf.my/myemacs ~/.myemacs
+ln -s ~/Public/conf.my/emacs-modes/modes ~/.emacs.d/
