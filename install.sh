c1c5902d63a9fd21a76be8f5708a01aa151b46b0
Quentin R <quentin@chirp.io>
2016-08-06 21:28:46 +0100

    Update emacs conf

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 git submodule update --init --recursive
-mkdir ~/.emacs.d
+mkdir -p ~/.emacs.d
 ln -fs ~/Public/conf.my/bashrc ~/.bashrc
 ln -fs ~/Public/conf.my/gitconfig ~/.gitconfig
 ln -fs ~/Public/conf.my/emacs ~/.emacs
 ln -fs ~/Public/conf.my/myemacs ~/.myemacs
 ln -fs ~/Public/conf.my/emacs-modes/modes ~/.emacs.d/
-ln -fs ~/Public/conf.my/i3config ~/.i3/config
+ln -fs ~/Public/conf.my/i3config ~/.config/i3/config
